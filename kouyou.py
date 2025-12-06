da93ef98afdbd787c5a0dd5ca68cfce3b795effa
Junichi0921 <nagayama.junichi@mail.canon>
2025-12-06 17:50:19 +0900

    2nd commit for fix bug

--- kouyou.py
+++ kouyou.py
@@ -2,38 +2,42 @@ import streamlit as st
 import pandas as pd
 import random
 
+st.title('こうゆう学園1問1答')
+# 問題データ(csv形式)読み込み
+df = pd.read_csv('4_M-18_こうゆう学園.csv')
+# シード値を固定し問題をシャッフル。ボタンを押すたびに再サンプリングを行なわないようにするための処置。
 value = st.sidebar.slider('問題シャッフルパターン', 0, 100, help='0はシャッフルなし')
 random.seed(value)
-
+# 教科ラジオボタン作成、ユニークな要素で列挙
 subject = st.sidebar.radio(
-    "教科",
-    ("理科", "社会")
+    df.columns[1],
+    tuple(df[df.columns[1]].unique())
 )
-st.title('こうゆう学園1問1答')
+# 年月ラジオボタン作成、ユニークな要素で列挙
 term = st.sidebar.radio(
     "第何回の問題にトライしますか？",
-    ("25年12月1週", "25年12月2週")
+    tuple(df[df.columns[0]].unique())
 )
+# ラジオボダンで選択された問題のみを抽出
+df = df[(df[df.columns[0]] == term) & (df[df.columns[1]] == subject)]
+# st.write(df)
 
 st.write('問題')
 # left_col, right_col = st.columns(2)
 
-df = pd.read_csv('4_M-18_こうゆう学園.csv')
-# st.write(df)
-df = df[(df['年月'] == term) & (df['教科'] == subject)]
-st.write(df)
-
-
+# 0以外の選択の場合は選択シード値に従ってランダムサンプリングを実施
 if value != 0:
     random_series = random.sample(range(len(df)), k=len(df))
 else:
     random_series = range(len(df))
 
 st.sidebar.write('答え')
+container = st.sidebar.container(border=True)
 button = []
+# ボタン作成
 for i in random_series:
     button.append(st.button(label=df.iloc[i, 2], type="secondary"))
-
-for _ in range(len(df)):
-    if button[_]:
-        st.sidebar.write(df.iloc[_, 3])
+# ボタンが押された場合答えを表示
+for j in range(len(df)):
+    if button[j]:
+        container.write(df.iloc[random_series[j], 3])
